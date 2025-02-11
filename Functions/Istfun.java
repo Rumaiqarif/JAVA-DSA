736df8ac8228c142c213928dc8010ebdf62d4cb8
Rumaiq Arif <rumaiqahmad@gmail.com>
2025-02-11 13:23:59 +0530

    Add files via upload

---
+++ Functions/Istfun.java
@@ -0,0 +1,13 @@
+public class Istfun {
+
+    public static void main(String[] args) {
+        String name = "Rumaiq";
+        greet(name);
+
+
+    }
+    static void greet(String naam){
+        System.out.println("Hello "+ naam + "!");
+
+    }
+}
